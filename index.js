83008a0c5058f4ffed1886a3b4cfe0513e790bf1
tamirdahan <tamir.dahan@ness-tech.co.il>
2024-08-17 13:12:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-const express = require('express');
-const app = express();
+#const express = require('express');
+#const app = express();
 const port = 3000;
 
-var cookieParser = require('cookie-parser')
-app.use(cookieParser())
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+#app.get('/', (req, res) => {
+#  res.send('Hello World!');
+#});
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
-});
+#app.listen(port, () => {
+#  console.log(`Server running at http://localhost:${port}/`);
+#});
