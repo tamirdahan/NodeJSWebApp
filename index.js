b1b629af2c7ee88ce13e5b68cf9f9779fbbf0e03
tamirdahan <tamir.dahan@ness-tech.co.il>
2024-08-18 15:20:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+const bodyparser = require('body-parser')
 
 const express = require('express');
 const app = express();
 const port = 3000;
+
+// Body-parser middleware
+app.use(bodyparser.urlencoded({ extended: true }))
+app.use(bodyparser.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -10,3 +15,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
+
